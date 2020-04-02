ecddc4713b988185dfc91eb118151e675f3dacf8
eromasters <evromastersx@gmail.com>
2020-04-02 14:43:32 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="row-1 py-5">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+<h3 > Наша студия предполагает услугу</h3>
+<h1>Дизайн </h1>
+</div>
+@yield('content')
+
+@endsection

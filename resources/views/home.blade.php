629a0bb4793e8e6f8e11ffb8ad2350a53001d82c
eromasters <evromastersx@gmail.com>
2020-04-06 15:10:44 +0300

    add footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+
 <section class="row-1 py-5">
 <div class="container">
-<div class="row">
-<div class="col-md-6">
-<h3 > Наша студия предполагает услугу</h3>
-<h1>Дизайн </h1>
+    <div class="row">
+        <div class="col-md-6">
+            <h3 > Наша студия предполагает услугу</h3>
+             <h1>Дизайн </h1>
+        </div>
+    </div>
 </div>
-@yield('content')
+</section>
+
+<section class="row-2 py-5 mt-5">
+    <div class="container main mb-5">
+    <h1> Main page</h1>
+    <div class="col-3">
+
+    </div>
+
+    </div>
+</section>
 
 @endsection

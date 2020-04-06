652e6cff26a9c1687ec8ad4962149810ea718e9a
eromasters <evromastersx@gmail.com>
2020-04-06 15:58:45 +0300

    add margin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,29 @@
 
 @section('content')
 
-<section class="row-1 py-5">
+<section class="row-1">
 <div class="container">
     <div class="row">
         <div class="col-md-6">
             <h3 > Наша студия предполагает услугу</h3>
-             <h1>Дизайн </h1>
+             <h1>Дизайн ногтей</h1>
+             <p> Хорошее настроение и постоянно желание полюбоваться своими ручками - чувства, которые знакомы каждой девушке, выходящей из салона после маникюра. Дизайн ногтей  гель лаком подчеркнет или же дополнит образ его обладательницы, а также  дает возможность воплотить практически любые фантазии.
+             </p><p>Желаете записаться на маникюр? А возможно желаете выполнить наращивание ногтей?</p>
+        </div>
+        <div class="col-md-6">
+        <img src="{{asset('assets/images')}}"
+
         </div>
     </div>
 </div>
 </section>
 
-<section class="row-2 py-5 mt-5">
+<section class="row-2 py-15 mt-5">
     <div class="container main mb-5">
-    <h1> Main page</h1>
-    <div class="col-3">
+        <h1> Main page</h1>
+        <div class="col-3">
 
-    </div>
+        </div>
 
     </div>
 </section>
